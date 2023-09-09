82ca854f2e5cb5cec1b025a00e206b0087af6e78
Vikas Chauhan <vcikas2626@gmail.com>
2023-09-09 14:24:35 +0530

    updates
    added hook to index.ts file,
    added dependencies accordingly
    updated publish.yml file

--- index.ts
+++ index.ts
@@ -1,3 +1,35 @@
-export const add = (a:number, b: number) =>{
-    return a+b;
+import { useEffect, useState, useCallback, useDebugValue } from "react";
+
+
+type UseLocalStorageHook<T> = [T, (value: T) => void];
+
+function useLocalStorage<T>(
+  key: string,
+  initialValue: T = "" as T
+): UseLocalStorageHook<T> {
+  const [value, setValue] = useState<T>(() => {
+    const existingValue = localStorage.getItem(key);
+    return existingValue ? JSON.parse(existingValue) : initialValue;
+  });
+
+  const setValueCallback = useCallback(
+   (newValue: T) => {
+      setValue(newValue);
+    },
+    [setValue]
+  );
+
+  useEffect(() => {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log(error)
+    }
+  }, [key, value]);
+
+  useDebugValue(value ?? 'loading...')
+
+  return [value, setValueCallback];
 }
+
+export default useLocalStorage;
